703b188fa6daf4c407479c09f1c6ba98bf2cdc29
James Henderson <james@pcr.me.uk>
2012-11-24 21:14:17 +0000

    Added the datomic sample data to an in-mem database

--- test/datomic_tree/query-test.clj
+++ test/datomic_tree/query-test.clj
@@ -1,10 +1,22 @@
 (ns datomic-tree.query-test
-  (:use datomic-tree.query)
-  (:require [datomic.api :as d]))
+  (:use datomic-tree.query
+        [datomic.api :only [q db] :as d]))
 
 (defonce url "datomic:mem://tree-test")
 
+(d/delete-database url)
 (d/create-database url)
 
 (defonce conn (d/connect url))
+
+(defonce schema (read-string (slurp "test/datomic/samples/seattle/seattle-schema.dtm")))
+
+(d/transact conn schema)
+
+(defonce data (map (fn [file-num] (-> (format "test/datomic/samples/seattle/seattle-data%d.dtm" file-num)
+                                      slurp
+                                      read-string))
+                   (range 2)))
+
+(doall (map #(d/transact conn %) data))
 
